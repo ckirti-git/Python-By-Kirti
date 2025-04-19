bf25a94913efe900a547b4b7c4d7fcb0ae2bd329
Kirti Chaudhari <ckirti.work@gmail.com>
2025-04-19 15:21:44 +0530

    Update Length_of_Longest_substring_without_repeating.py

--- Strings/Length_of_Longest_substring_without_repeating.py
+++ Strings/Length_of_Longest_substring_without_repeating.py
@@ -19,3 +19,27 @@ for k in range(len(s)):
 
 
 print(max_len,' = ',max_sub)
+
+
+#------------------Using Sliding window-------------------------------
+
+s = "abba"
+
+start = 0
+max_len = 0
+max_sub = ""
+char_index = {}
+
+for end in range(len(s)):
+    if s[end] in char_index and char_index[s[end]] >= start:
+        start = char_index[s[end]] + 1  # Move start ahead of the repeated char
+
+    char_index[s[end]] = end  # Update latest index of character
+
+    window_len = end - start + 1
+    if window_len > max_len:
+        max_len = window_len
+        max_sub = s[start:end+1]
+    
+
+print(max_len, '=', max_sub)
